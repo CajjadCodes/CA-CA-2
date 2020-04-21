fb2fdde556abd9769874c1a8d68677509372d985
CajjadCodes <62202221+CajjadCodes@users.noreply.github.com>
2020-04-21 18:29:48 +0430

    Update TBmips.v

--- TBmips.v
+++ TBmips.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ns
 module TBmips();
 
 	reg clk = 0, rst = 0;
@@ -7,14 +8,16 @@ module TBmips();
 		.rst(rst)
 		);
 	
-	always #300 clk = ~clk;
+	integer InstNum = 0;
+	always #150 clk = ~clk;
+	always #300 InstNum = InstNum + 1;
 	initial begin
 		#100
 		rst = 1;
 		#300
 		rst = 0;
 	
-		#4000
+		#400000
 		$stop;
 	end
 
