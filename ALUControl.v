796a2358c9357f18035d21469dcef42cb0571e4a
CajjadCodes <62202221+CajjadCodes@users.noreply.github.com>
2020-04-21 11:40:09 +0430

    Fixing Combinational style

--- ALUControl.v
+++ ALUControl.v
@@ -3,6 +3,7 @@ input [1:0]ALUOp;
 input [5:0]func;
 output reg [2:0]ALUOperation;
 	always@(ALUOp or func)begin
+		ALUOperation = 3'b000;
 		case(ALUOp)
 			2'b00: ALUOperation=3'b010;
 			2'b01: ALUOperation=3'b011;
@@ -13,9 +14,11 @@ output reg [2:0]ALUOperation;
 					6'b100100: ALUOperation=3'b000;
 					6'b100101: ALUOperation=3'b001;
 					6'b101010: ALUOperation=3'b111;
+					default: ALUOperation = 3'b000;
 				endcase
 			end
 			2'b11: ALUOperation=3'b000;
+			default: ALUOperation = 3'b000;
 		endcase
 	end
 endmodule
